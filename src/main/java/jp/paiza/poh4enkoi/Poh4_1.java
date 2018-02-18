2213c73f61d7936524ce52a9dd7363743021f1b7
r-jimano <r-jimano@users.noreply.github.com>
2018-02-18 22:35:10 +0900

    add poh4

---
+++ src/main/java/jp/paiza/poh4enkoi/Poh4_1.java
@@ -0,0 +1,19 @@
+package jp.paiza.poh4enkoi;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Poh4_1 {
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int cnt = Integer.parseInt(br.readLine());
+		int stock, sum = 0;
+
+		// 商品の各在庫数を合計
+		for (int i = 0; i < cnt; i++) {
+			stock = Integer.parseInt(br.readLine());
+			sum += stock;
+		}
+		System.out.println(sum);
+	}
+}
