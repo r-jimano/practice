34da75d57ad33387429fd13ea5d593b85230233b
r-jimano <r-jimano@users.noreply.github.com>
2018-05-09 11:33:19 +0900

    format

--- src/main/java/CheckDigitSample.java
+++ src/main/java/CheckDigitSample.java
@@ -4,27 +4,27 @@ import java.util.*;
  * modulus10 wait 3-1 (MOD 10)
  */
 public class CheckDigitSample {
-    @SuppressWarnings("resource")
-    public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	int modulus = 10;
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int modulus = 10;
 
-	StringBuilder sb = new StringBuilder(sc.next());
-	String rev = sb.append(0).reverse().toString();
-	char[] arr = rev.toCharArray();
-	int oddTotal = 0;
-	int evenTotal = 0;
+		StringBuilder sb = new StringBuilder(sc.next());
+		String rev = sb.append(0).reverse().toString();
+		char[] arr = rev.toCharArray();
+		int oddTotal = 0;
+		int evenTotal = 0;
 
-	for (int i = 0; i < arr.length; i++) {
-	    if ((i + 1) % 2 == 0) {
-		evenTotal += arr[i] - 48;
-	    } else {
-		oddTotal += (int) arr[i] - 48;
-	    }
+		for (int i = 0; i < arr.length; i++) {
+			if ((i + 1) % 2 == 0) {
+				evenTotal += arr[i] - 48;
+			} else {
+				oddTotal += (int) arr[i] - 48;
+			}
+		}
+
+		int digit = 10 - (oddTotal * 3 + evenTotal) % modulus;
+		System.out.println(digit);
 	}
-
-	int digit = 10 - (oddTotal * 3 + evenTotal) % modulus;
-	System.out.println(digit);
-    }
 
 }
--- src/main/java/ConvFileEncode.java
+++ src/main/java/ConvFileEncode.java
@@ -11,52 +11,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ConvFileEncode {
-    public static void main(String[] args) {
-	try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-	    System.out.println("Dir : ");
-	    Path path = Paths.get(br.readLine());
-	    convToUTF8(path, "SJIS");
-	} catch (IOException e) {
-	    e.printStackTrace();
+	public static void main(String[] args) {
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+			System.out.println("Dir : ");
+			Path path = Paths.get(br.readLine());
+			convToUTF8(path, "SJIS");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Finished.");
 	}
-	System.out.println("Finished.");
-    }
 
-    /**
-     * 指定されたディレクトリ配下に存在するテキストファイルのエンコードをUTF-8に変更します。
-     * 
-     * @param dir
-     * @param oldCharset
-     * @throws IOException
-     */
-    static void convToUTF8(Path dir, String oldCharset) throws IOException {
-	Files.walk(dir, FileVisitOption.FOLLOW_LINKS).filter(path -> path.toFile().isFile())
-		.forEach(path -> _convToUTF8(path, oldCharset));
-    }
-
-    /**
-     * テキストファイルのエンコードをUTF-8に変更します。
-     * 
-     * @param txt
-     * @param oldCharset
-     * @return
-     * @throws IOException
-     */
-    static void _convToUTF8(Path txt, String oldCharset) {
-	// Files.readAllLinesでCharset.forName()を利用してデコード文字セットを指定すると怒られた・・・
-	try (BufferedReader br = new BufferedReader(
-		new InputStreamReader(Files.newInputStream(txt), oldCharset))) {
-	    List<String> lines = new ArrayList<>();
-	    String line = null;
-	    while ((line = br.readLine()) != null) {
-		lines.add(line);
-	    }
-	    Path tmpPath = Files.createTempFile(txt.getParent(), txt.getFileName().toString(), null);
-	    Files.copy(txt, tmpPath, StandardCopyOption.REPLACE_EXISTING);
-	    Files.write(txt, lines, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
-	    Files.deleteIfExists(tmpPath);
-	} catch (IOException e) {
-	    e.printStackTrace();
+	/**
+	 * 指定されたディレクトリ配下に存在するテキストファイルのエンコードをUTF-8に変更します。
+	 * 
+	 * @param dir
+	 * @param oldCharset
+	 * @throws IOException
+	 */
+	static void convToUTF8(Path dir, String oldCharset) throws IOException {
+		Files.walk(dir, FileVisitOption.FOLLOW_LINKS).filter(path -> path.toFile().isFile())
+				.forEach(path -> _convToUTF8(path, oldCharset));
 	}
-    }
+
+	/**
+	 * テキストファイルのエンコードをUTF-8に変更します。
+	 * 
+	 * @param txt
+	 * @param oldCharset
+	 * @return
+	 * @throws IOException
+	 */
+	static void _convToUTF8(Path txt, String oldCharset) {
+		// Files.readAllLinesでCharset.forName()を利用してデコード文字セットを指定すると怒られた・・・
+		try (BufferedReader br = new BufferedReader(new InputStreamReader(Files.newInputStream(txt), oldCharset))) {
+			List<String> lines = new ArrayList<>();
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+			Path tmpPath = Files.createTempFile(txt.getParent(), txt.getFileName().toString(), null);
+			Files.copy(txt, tmpPath, StandardCopyOption.REPLACE_EXISTING);
+			Files.write(txt, lines, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+			Files.deleteIfExists(tmpPath);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
