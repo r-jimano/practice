53cf4cff8912522210e809ac45113f30893e0bde
r-jimano <r-jimano@users.noreply.github.com>
2019-07-31 08:25:49 +0900

    アルゴリズムをはじめよう　SearcherExample

---
+++ src/main/java/jp/co/impress/book/algorithm/search/SearcherExample.java
@@ -0,0 +1,35 @@
+package jp.co.impress.book.algorithm.search;
+
+public class SearcherExample {
+
+	int linearSearch(int[] arr, int target) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == target) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	int binarySearch(int[] arr, int target) {
+		// 探索範囲
+		int begin = 0;
+		int end = arr.length;
+
+		while (begin < end) {
+			int center = (begin + end) / 2;
+			if (arr[center] == target) {
+				return center;
+			} else {
+				// 検索範囲を中央の前or後ろに絞り込んで、 以下繰り返し
+				if (arr[center] > target) {
+					begin = center + 1;
+				} else {
+					end = center - 1;
+				}
+			}
+		}
+		return -1;
+	}
+
+}
---
+++ src/test/java/jp/co/impress/book/algorithm/search/SearcherExampleTest.java
@@ -0,0 +1,19 @@
+package jp.co.impress.book.algorithm.search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SearcherExampleTest {
+
+	@Test
+	void testLinearSearch() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testBinarySearch() {
+		fail("まだ実装されていません");
+	}
+
+}
