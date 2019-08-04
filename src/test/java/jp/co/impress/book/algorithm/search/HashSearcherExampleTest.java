1761f2fd5d347ca4efecc88db37bf49bee682a57
r-jimano <r-jimano@users.noreply.github.com>
2019-08-04 20:51:37 +0900

    Revert "Revert "add HashSearcherExampleTest""
    This reverts commit 14ff18496b05ec220a17b9ba17a31d005b5bb9fa.

---
+++ src/test/java/jp/co/impress/book/algorithm/search/HashSearcherExampleTest.java
@@ -0,0 +1,27 @@
+package jp.co.impress.book.algorithm.search;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class HashSearcherExampleTest {
+	HashSearcherExample searcher;
+
+	@BeforeEach
+	void beforeTest() {
+		searcher = new HashSearcherExample();
+	}
+
+	@Test
+	void testHashSearch() {
+		int[] arr = {12,25,36,20,30,8,42};
+		searcher.createHashArray(arr);
+
+		int actual = searcher.hashSearch(arr, 20);
+		assertTrue(actual != -1 , "見つかりませんでした");
+		actual = searcher.hashSearch(arr, 1);
+		assertTrue(actual == -1 , "いないはず");
+	}
+
+}
--- src/test/java/jp/co/impress/book/algorithm/search/SearcherExampleTest.java
+++ src/test/java/jp/co/impress/book/algorithm/search/SearcherExampleTest.java
@@ -8,11 +8,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SearcherExampleTest {
-
 	SearcherExample searcher;
 
 	@BeforeEach
-	void before() {
+	void beforeTest() {
 		searcher = new SearcherExample();
 	}
 
@@ -36,6 +35,7 @@ class SearcherExampleTest {
 	void testBinarySearch() {
 		int[] arr = { 11, 13, 17, 19, 23, 29, 31 };
 		Arrays.sort(arr);
+
 		assertEquals(searcher.linearSearch(arr, 11), 0);
 		int actual = arr.length - 1;
 		assertEquals(searcher.linearSearch(arr, 31), actual);
@@ -43,6 +43,7 @@ class SearcherExampleTest {
 
 		int[] arr2 = { 11, 13, 17, 19, 23, 29 };
 		Arrays.sort(arr);
+
 		assertEquals(searcher.linearSearch(arr2, 11), 0);
 		assertEquals(searcher.linearSearch(arr2, 29), arr2.length - 1);
 		assertEquals(searcher.linearSearch(arr2, 35), -1);
