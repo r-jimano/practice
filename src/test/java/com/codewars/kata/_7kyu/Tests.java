8579c8b7aeb536b36e7fc1127c9843e52ddc31fe
r-jimano <r-jimano@users.noreply.github.com>
2019-08-23 21:14:05 +0900

    mod for junit.jupiter

--- src/test/java/com/codewars/kata/_7kyu/Tests.java
+++ src/test/java/com/codewars/kata/_7kyu/Tests.java
@@ -1,7 +1,7 @@
 package com.codewars.kata._7kyu;
 
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Tests {
 	@Test
--- src/test/java/com/codewars/kata/_7kyu/isogramTest.java
+++ src/test/java/com/codewars/kata/_7kyu/isogramTest.java
@@ -1,6 +1,5 @@
 package com.codewars.kata._7kyu;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
