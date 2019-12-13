d7e43272d98a7df6aa72df3af11e446391c75f93
r-jimano <r-jimano@users.noreply.github.com>
2019-12-13 18:16:03 +0900

    correct bubbleSort

--- src/main/java/jp/co/impress/book/algorithm/SortExample.java
+++ src/main/java/jp/co/impress/book/algorithm/SortExample.java
@@ -21,12 +21,12 @@ public class SortExample {
 	void bubbleSort(int[] arr) {
 		for (int i = 0; i < arr.length - 1; i++) {
 			int tmp = 0;
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) {
-					tmp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = tmp;
-				}
+			for (int j = arr.length - 1; j >= i + 1; j--) {
+				if (arr[j] < arr[j - 1]) {
+					tmp = arr[j];
+					arr[j] = arr[j-1];
+					arr[j-1] = tmp;
+				  }
 			}
 		}
 	}
